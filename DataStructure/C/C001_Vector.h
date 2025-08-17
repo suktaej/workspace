25c870a1b7d5a5d4d49f14d296a79f04d39a4933
code-server <suktaej@gmail.com>
2025-08-17 17:22:20 +0900

    std::vector implementation for c

--- DataStructure/C/C001_Vector.h
+++ DataStructure/C/C001_Vector.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct 
+typedef struct
 {
     int* data;      // 입력될 데이터
     int size;       // 현재 저장된 원소의 개수
@@ -12,27 +13,32 @@ typedef struct
 
 } DynamicArray;
 
-void InitArray(DynamicArray* arr,int cap);
+void InitArray(DynamicArray* arr, int cap);
 void FreeArray(DynamicArray* arr);
-void push_back(DynamicArray* arr,int value);
+void Resizing(DynamicArray* arr);
+void PrintArray(DynamicArray* arr);
+
+void push_back(DynamicArray* arr, int value);
 void pop_back(DynamicArray* arr);
 void arr_insert(DynamicArray* arr, int idx, int value);
 //void arr_emplace(DynamicArray* arr, int idx, int value);
+
 void arr_erase(DynamicArray* arr, int idx);
 void arr_remove(DynamicArray* arr, int value);
-
 void arr_clear(DynamicArray* arr);
-void arr_swap(DynamicArray* arr,DynamicArray* swapArr);
-int* arr_begin(DynamicArray* arr);
+
+int* arr_begin(DynamicArray* arr); // 주소 반환
 int* arr_end(DynamicArray* arr);
-int* arr_rbegin(DynamicArray* arr);
-int* arr_rend(DynamicArray* arr);
-int* arr_at(DynamicArray* arr,int idx);
-int* arr_front(DynamicArray* arr);
-int* arr_back(DynamicArray* arr);
-int get_size(DynamicArray* arr);
-int get_capacity(DynamicArray* arr);
-void Resizing(DynamicArray *arr);
-void PrintArray(DynamicArray *arr);
+
+int arr_front(DynamicArray* arr);  // 값 반환
+int arr_back(DynamicArray* arr);
+int arr_at(DynamicArray* arr, int idx);
+
+int arr_find(DynamicArray* arr, int value);   // 값의 인덱스 반환, 없으면 -1
+bool arr_contains(DynamicArray* arr, int value); // 존재 여부 확인
+
+//int* arr_rbegin(DynamicArray* arr);
+//int* arr_rend(DynamicArray* arr);
+
 
 #endif //DYNAMIC_ARRAY_H
