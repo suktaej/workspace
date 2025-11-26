c80b3ce540637c348afb90c8f5f2455c308532b4
code-server <suktaej@gmail.com>
2025-11-26 15:46:24 +0900

    Fixed function

--- 02_Programming/CodeTest/GeometricArithmetic.cpp
+++ 02_Programming/CodeTest/GeometricArithmetic.cpp
@@ -5,17 +5,18 @@ void CheckProgression(std::vector<int> vec)
 {
     int a = vec[0];
     int b = vec[1];
+    int res;
 
     int ratio = b - a;
 
     if(b+ratio==vec[2])
     {
-        vec.back() + ratio;
+        res = vec.back() + ratio;
     }
     else
     {
         ratio = b / a;
-        vec.back() * ratio;
+        res = vec.back() * ratio;
     }
 }
 
