8818ed3d5c033d02d2b432c208a29ebbaa8c5055
stj_pc <suktaej@gmail.com>
2025-12-07 01:29:24 +0900

    Added Duplicate and Replace

---
+++ 02_Programming/CodeTest/025_RemoveDuplicate.cpp
@@ -0,0 +1,105 @@
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+int MakeTriangleUsingVector(std::vector<int> sides)
+{
+    int most = std::max(sides[0], sides[1]);
+    int less = std::min(sides[0], sides[1]);
+
+    // 1. vector 저장
+    std::vector<int> vecLength;
+    for (int i = most - less + 1; i <= most; ++i)
+        vecLength.push_back(i);
+
+    for (int i = most + 1; most + less > i; ++i)
+        vecLength.push_back(i);
+
+    int count = 0;
+
+    for(int i=0;i<vecLength.size();++i)
+    {
+        ++count;
+        
+        for(int j=i+1;j<vecLength.size();++j)
+        {
+            if(vecLength[i]==vecLength[j])
+            {
+                --count;
+                break;
+            }
+        }
+    }
+    
+    return count;
+}
+
+int MakeTriangleUsingSet(std::vector<int> sides)
+{
+    int most = std::max(sides[0], sides[1]);
+    int less = std::min(sides[0], sides[1]);
+    
+    // 2. unordered_set (중복무시)
+    std::unordered_set<int> setLength;
+
+    for (int i = most - less + 1; i <= most; ++i)
+        setLength.insert(i);
+
+    for (int i = most + 1; most + less > i; ++i)
+        setLength.insert(i);
+
+    return setLength.size();
+}
+
+int MakeTriangleUsingBool(std::vector<int> sides)
+{
+    int most = std::max(sides[0], sides[1]);
+    int less = std::min(sides[0], sides[1]);
+
+    // 3. bool vector를 통한 중복체크
+    std::vector<bool> dupCheck(most * 10, false);
+    std::vector<int> vecLength;
+    
+    for (int i = most - less + 1; i <= most; ++i)
+    {
+        if (false == dupCheck[i])
+        {
+            dupCheck[i] = true;
+            vecLength.push_back(i);
+        }
+    }
+
+    for (int i = most + 1; most + less > i; ++i)
+    {
+        if (false == dupCheck[i])
+        {
+            dupCheck[i] = true;
+            vecLength.push_back(i);
+        }
+    }
+
+    return vecLength.size();
+}
+
+int MakeTriangleUsingUnique(std::vector<int> sides)
+{
+    int most = std::max(sides[0], sides[1]);
+    int less = std::min(sides[0], sides[1]);
+
+    std::vector<int> length;
+    for (int i = most - less + 1; i <= most; ++i)
+        length.push_back(i);
+
+    for (int i = most + 1; most + less > i; ++i)
+        length.push_back(i);
+
+    std::sort(length.begin(), length.end());
+    
+    // 4. std::unique
+    // 연속된 중복들을 탐지하고 맨 뒤로 이동시킴(정렬필요)
+    // 예) 3 3 4 4 5 5 가 입력될 경우 3 4 5 | 3 4 5
+    // 반환되는 위치는 |
+    length.erase(std::unique(length.begin(), length.end()), length.end());
+
+    return length.size();
+}
---
+++ 02_Programming/CodeTest/026_Replace.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+long long VectorReplace(std::string numbers) {
+    
+    const std::vector<std::pair<std::string, std::string>> table = {
+        {"zero", "0"}, {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"},
+        {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}
+    };
+    
+    for(auto& [word,digit] : table)
+    {
+        size_t pos = 0;
+        
+        // string.find(탐색할문자열,탐색시작위치)
+        while((pos = numbers.find(word, pos)) != std::string::npos)
+        {
+            // replace(치환시작위치,제거할용량,삽입할문자열)
+            numbers.replace(pos,word.size(),digit);
+            pos += 1;
+        }
+    }
+    return std::stoi(numbers);
+}
+
+int MapReplace(const std::string& numbers) {
+    std::unordered_map<std::string, int> dict = {
+        {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
+
+    // std::string result;
+    int result = 0;
+    std::string buffer;
+
+    for (char c : numbers) 
+    {
+        // buffer로 복사
+        buffer.push_back(c);
+
+        // Map function
+        // count(inp) : key를 탐색(존재하면 true 반환) - 존재여부만 확인
+        // find(inp) : key를 탐색(존재하면 iterator반환) - 존재하는 값을 사용
+        if (dict.count(buffer)) 
+        {
+            // result.push_back(dict[buffer]);
+            result = result * 10 + dict[buffer];
+            buffer.clear();
+        }
+    }
+
+    return result;
+}
