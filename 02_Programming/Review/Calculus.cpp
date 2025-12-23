7637272d1eb97c9acffbdf16ad81371cb6dcd365
stj_laptop <suktaej@gmail.com>
2025-12-23 17:53:59 +0900

    Added Calculus

---
+++ 02_Programming/Review/Calculus.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <functional>
+#include <cmath>
+#include <iostream>
+
+float DegToRad(float deg)
+{
+    constexpr float PI = 3.141592f;
+    constexpr float RAD = 180.f;
+
+    return deg * (PI/RAD);
+}
+
+template <typename T>
+double Derivative(T&& func, double x)
+{
+    constexpr double h = 1e-6;
+    return (func(x + h) - func(x - h)) / (2.0 * h);
+}
+
+template <typename T>
+double Integrate(T&& func, double a, double b, int n)
+{
+    double h = (b - a) / n;
+    // 한 구간은 h/2(f(x)+f(x+1))
+    // 전 구간을 합할 경우
+    // h/2{f(x0)+2f(x1)+2f(x2)...+2f(xn-1)+f(xn)}
+    // 첫 구간과 마지막 구간의 1회 합이 추가로 필요
+    double sum = 0.5 * (func(a) + func(b));
+
+    for (int i = 1; i < n; ++i)
+        sum += func(a + i * h);
+
+    return sum * h;
+}
+
+int main()
+{
+    auto f = [](double x) { return x * x + std::sin(x); };
+    double d = Derivative(f, 1.f);
+    std::cout<<d;
+    return 0;
+}
