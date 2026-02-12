5adce0325d4870a04cc4a4e0b2226b5ab4f087f9
stj_laptop <suktaej@gmail.com>
2026-02-12 18:01:27 +0900

    Updated dijkstrra

--- 02_Programming/CodeTest/112_Dijkstra_DFS.cpp
+++ 02_Programming/CodeTest/112_Dijkstra_DFS.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
+constexpr int INF = 1e9;
 int n, e;
 std::vector<std::vector<std::pair<int,int>>> gNode;
 
@@ -13,8 +15,40 @@ void input()
     {
         int u, v, w;
         std::cin>>u>>v>>w;
-        gNode[u].push_back({v, w});
-        gNode[v].push_back({u, w});
+        gNode[u].push_back({w, v});
+        gNode[v].push_back({w, u});
+    }
+}
+
+int dfs(int src, int dest)
+{
+    std::vector<int> dist(n,INF);
+    std::vector<int> parent(n);
+    std::priority_queue<std::pair<int,int>, std::vector<std::pair<int,int>>, std::greater<>> minHeap;
+
+    for(int i=0;i<n;++i)
+        parent[i] = i;
+
+    dist[src] = 0;
+    minHeap.push({dist[src],src});
+
+    while(!minHeap.empty())
+    {
+        auto [curDist, curNode] = minHeap.top();
+        minHeap.pop();
+
+        if(curNode == dest)
+            break;
+        
+        // 중복된 vertex가 삽입가능하므로
+        // outdate entry skip
+        if(dist[curNode] < curDist)
+            continue;
+
+        for(const auto& next : gNode[curNode])
+        {
+            auto [nextDist, nextNode] = next;
+        }
     }
 }
 
