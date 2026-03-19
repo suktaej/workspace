c7d7bfd51e90312d2a5546bbf67aa1e13df6b10f
stj_laptop <suktaej@gmail.com>
2026-03-19 09:48:22 +0900

    Update travel

--- 02_Programming/CodeTest/203_Travel.cpp
+++ 02_Programming/CodeTest/203_Travel.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <stack>
+#include <string>
 #include <vector>
 
-char tree[128] = {'1', '+', '4', '-' ,'7'};
+std::string tree[128] = {};
 
 int parent(int i) { return (i - 1) / 2; }
 int left(int i)   { return 2 * i + 1; }
@@ -9,7 +11,7 @@ int right(int i)  { return 2 * i + 2; }
 
 void inOrder(int i)
 {
-    int ts = sizeof(tree)/sizeof(int);
+    int ts = sizeof(tree)/sizeof(tree[0]);
     
     if(i >= ts)
         return;
@@ -19,20 +21,74 @@ void inOrder(int i)
     inOrder(right(i));
 }
 
-void inOrder(int i)
+std::stack<int> operend;
+
+void postCal(int i)
 {
-    int ts = sizeof(tree)/sizeof(int);
+    std::string c = tree[i];
+    if(c == "+" || c == "-" || c == "*" || c == "/" || c == "%")
+    {
+        int rightValue = operend.top();
+        operend.pop();
+        int leftValue = operend.top();
+        operend.pop();
+
+        if(c == "+") operend.push(leftValue + rightValue);
+        else if(c == "-") operend.push(leftValue - rightValue);
+        else if(c == "*") operend.push(leftValue * rightValue);
+        else if(c == "/") operend.push(leftValue / rightValue);
+        else if(c == "%") operend.push(leftValue % rightValue);
+    }
+    else
+        operend.push(std::stoi(c));
+}
+
+void postOrder(int i)
+{
+    int ts = sizeof(tree)/sizeof(tree[0]);
     
     if(i >= ts)
         return;
 
-    inOrder(left(i));
+    postOrder(left(i));
+    postOrder(right(i));
+    postCal(i);
+}
+
+int preCal(int i)
+{
+    if(i>=128 || tree[i]=="")
+        return 0;
+
+    std::string c = tree[i];
+    if(c == "+" || c == "-" || c == "*" || c == "/" || c == "%")
+    {
+        int leftVal = preCal(left(i));
+        int rightVal = preCal(right(i));
+
+        if (c == "+") return leftVal + rightVal;
+        if (c == "-") return leftVal - rightVal;
+        if (c == "*") return leftVal * rightVal;
+        if (c == "/") return leftVal / rightVal;
+        if (c == "%") return leftVal % rightVal;
+    }
+    else
+        return std::stoi(c);
+}
+
+void preOrder(int i)
+{
+    int ts = sizeof(tree)/sizeof(tree[0]);
+    
+    if(i >= ts)
+        return;
+
     std::cout<<tree[i];
-    inOrder(right(i));
+    preOrder(left(i));
+    preOrder(right(i));
 }
 
 int main()
 {
-    inOrder(0);
     return 0;
 }
